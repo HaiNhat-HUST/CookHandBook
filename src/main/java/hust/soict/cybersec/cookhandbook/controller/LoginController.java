bacc0a036b076e2a7c6248e059804e0a584cebfc
HaiNhat-HUST <nhat.lh225583@sis.hust.edu.vn>
2024-12-09 21:41:36 +0700

    modify controller for register and login

--- src/main/java/hust/soict/cybersec/cookhandbook/controller/LoginController.java
+++ src/main/java/hust/soict/cybersec/cookhandbook/controller/LoginController.java
@@ -1,12 +1,24 @@
 package hust.soict.cybersec.cookhandbook.controller;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     private TextField usernameField;
@@ -29,5 +41,16 @@ public class LoginController {
         // Xử lý logic đăng nhập tại đây
         System.out.println("Username: " + username + ", Password: " + password);
     }
+
+    //switch to register
+    @FXML
+    private void handleRegister(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/hust/soict/cybersec/cookhandbook/view/register.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
 
--- src/main/java/hust/soict/cybersec/cookhandbook/controller/RegisterController.java
+++ src/main/java/hust/soict/cybersec/cookhandbook/controller/RegisterController.java
@@ -1,12 +1,24 @@
 package hust.soict.cybersec.cookhandbook.controller;
 
 import hust.soict.cybersec.cookhandbook.App;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class RegisterController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     private TextField usernameField;
@@ -39,8 +51,13 @@ public class RegisterController {
         System.out.println("Registering user: " + username);
     }
 
-//    @FXML
-//    private void switchToLogin() {
-//        App.switchScene("login.fxml");
-//    }
+    //switch to log in
+    @FXML
+    private void handleLogin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/hust/soict/cybersec/cookhandbook/view/login.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
